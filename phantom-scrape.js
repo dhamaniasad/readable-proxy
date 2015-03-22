a7e2aa561bf8080ad1c70a6e860cc785ba079a84
Asad Dhamani <dhamaniasad@gmail.com>
2015-03-22 21:51:48 +0530

    Update phantom-scrape.js

--- phantom-scrape.js
+++ phantom-scrape.js
@@ -16,8 +16,7 @@ if (!url) {
 
 page.open(url, function(status) {
   if (status !== "success") {
-    console.log(status);
-    output({error: "Unable to open " + url});
+    output({error: "Unable to open " + url, status: status});
     return phantom.exit();
   }
   page.injectJs("vendor/Readability.js");
